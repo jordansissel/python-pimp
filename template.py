688116001006bc0e83c832d1fbfd89a2ba6a53f3
Jordan Sissel <jls@semicomplete.com>
2005-12-15 18:50:05 +0000

    - replicate() works mostly.
    git-svn-id: https://semicomplete.googlecode.com/svn/pimp@191 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- template.py
+++ template.py
@@ -7,12 +7,40 @@ class Template:
 	def __init__(self, filename):
 		self.dom = minidom.parse(filename);
 
+	def getElementById(self, id, doc=None):
+		if (doc is None):
+			doc = self.dom.documentElement
+		return xpath.Evaluate("/html/body//*[@id='%s']" % id, doc)[0]
+
 	def setTextById(self, id, text):
-		element = xpath.Evaluate("/html/body//*[@id='content']", self.dom.documentElement)[0]
+		element = self.getElementById(id)
 		for el in filter(lambda x: x.nodeType == 3, element.childNodes):
 			element.removeChild(el)
 		element.appendChild(self.dom.createTextNode(text))
 
+	def replicate(self, id, data):
+		original = self.getElementById(id)
+		parent = original.parentNode
+		data.reverse()
+		print "Data: %s" % data
+		for d in data:
+			parent.appendChild(self.replicateElement(original, d))
+
+		# Remove the template
+		#print "Removing %s from %s" % (original.nodeName, parent.nodeName)
+		parent.removeChild(original)
+
+	def replicateElement(self, element, data):
+		newelement = element.cloneNode(1)
+		x = 0
+
+		for k in data.keys():
+			el = self.getElementById(k, newelement)
+			el.setAttribute("id", "dup.%d" % x)
+			el.appendChild(self.dom.createTextNode(data[k]))
+			x = x + 1
+
+		return newelement
 
 	def output(self, writer):
 		self.dom.writexml(writer);
@@ -20,6 +48,5 @@ class Template:
 if __name__ == "__main__":
 	import sys
 	t = Template("test/layout.html")
-	t.test()
-	#t.setTextById("content", "one two three")
-	#t.output(sys.stdout)
+	t.replicate("row", [ {"one": "onetest", "two": "twotest"}, {"one": "threetest", "two": "fourtest" }])
+	t.output(sys.stdout)

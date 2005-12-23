179121525a74fb0f2ce5bcdcf924e72e0fb8c3ad
Jordan Sissel <jls@semicomplete.com>
2005-12-23 01:05:19 +0000

    - Effect.Zoom
    git-svn-id: https://semicomplete.googlecode.com/svn/pimp@216 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- static/effects.js
+++ static/effects.js
@@ -1,5 +1,33 @@
 
+function dbg(txt) {
+	document.body.appendChild(mktext(txt + " | "));
+}
 
+function getOffset(el,prop) {
+	var o;
+	if (prop == "width") o = el.offsetWidth;
+	else if (prop == "height") o = el.offsetHeight;
+	else if (prop == "top") o = el.offsetTop;
+	else if (prop == "left") o = el.offsetLeft;
+
+	return parseInt(o);
+}
+
+function clone(o) {
+	var x;
+	var type = typeof(o);
+
+	if (type == "object") {
+		x = {};
+		for (var i in o) {
+			x[i] = clone(o[i]);
+		}
+	} else {
+		x = o;
+	}
+
+	return x;
+}
 
 Effect = {
 	'Fade': function (obj, time, func) {
@@ -53,5 +81,62 @@ Effect = {
 						});
 	},
 
+	'Zoom': function(obj, time, opts, func) {
+
+		if (!opts["width"] || !opts["height"])
+			return;
+
+		opts["to"] = opts["width"];
+		opts["prop"] = "width";
+		Effect.ZoomElement(obj, time, clone(opts), func);
+
+		opts["to"] = opts["height"];
+		opts["prop"] = "height";
+		Effect.ZoomElement(obj, time, clone(opts), func);
+
+		opts["to"] = parseInt(obj.style.left) - (opts["width"] / 2);
+		opts["prop"] = "left";
+		Effect.ZoomElement(obj, time, clone(opts), func);
+
+		opts["to"] = parseInt(obj.style.top) - (opts["height"] / 2);
+		opts["prop"] = "top";
+		Effect.ZoomElement(obj, time, clone(opts), func);
+
+	},
+
+	'ZoomElement': function(obj, time, opts, func) {
+		if (!opts["max"])
+			opts["max"] = .20;
+
+		if (opts["max"] < 0)
+			return;
+
+		var offset;
+		var cur = obj.style[opts["prop"]];
+		if (!cur)
+			cur = getOffset(obj, opts["prop"]);
+
+		cur = parseInt(cur);
+
+		var delta = cur - opts["to"]
+		if (delta < 0)
+			offset = delta * opts["max"];
+		else
+			offset = delta * opts["max"];
+
+		if (opts["appear"]) {
+			obj.style.opacity = 0
+			Effect.Appear(obj, time*2);
+		}
+		var accel = new Accelimation(obj.style, opts["prop"], opts["to"] - offset, time, 1);
+		accel.onend = function() {
+			var x;
+			x = new Accelimation(obj.style, opts["prop"], opts["to"], time, 1);
+			x.onend = function() {};
+			x.start();
+		}
+		accel.start();
+
+	},
 }
 

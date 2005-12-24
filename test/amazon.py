63583556c453315b4cd49c2eadc4d9e86559ad09
Jordan Sissel <jls@semicomplete.com>
2005-12-24 06:48:12 +0000

    git-svn-id: https://semicomplete.googlecode.com/svn/pimp@220 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- test/amazon.py
+++ test/amazon.py
@@ -6,39 +6,32 @@ import sgmllib
 from xml import xpath
 import re
 
-amazonurl = "http://webservices.amazon.com/onca/xml"
+def getalbumcover(args):
+	amazonurl = "http://webservices.amazon.com/onca/xml"
+	params = {
+		'Service': "AWSECommerceService",
+		'AWSAccessKeyId': "03TSBYPH33N0PXE5RSR2",
+		'SearchIndex': "Music",
+		'Operation': "ItemSearch",
+		'Artist': args["artist"],
+		'Title': args["album"],
+	}
+	
+	args = urllib.urlencode(params)
+	url = "%s?%s" % (amazonurl, args)
 
-params = {
-	'Service': "AWSECommerceService",
-	'AWSAccessKeyId': "03TSBYPH33N0PXE5RSR2",
-	'SearchIndex': "Music",
-	'Operation': "ItemSearch",
-	'Artist': "Blind Guardian",
-	'Title': "A Night At The Opera",
-}
+	u1 = urllib.urlopen(url);
+	dom = minidom.parseString(u1.read());
 
-args = urllib.urlencode(params)
-url = "%s?%s" % (amazonurl, args)
+	albumpage = xpath.Evaluate("//Item/DetailPageURL", dom)[0].childNodes[0].nodeValue;
+	u2 = urllib.urlopen(albumpage);
+	m = re.compile("amz_js_PopWin\('(http://[^']+)");
+	imgurl = filter(lambda x: not re.search("/images/", x) is None, m.findall(u2.read()))[0]
+	u3 = urllib.urlopen(imgurl);
+	matches = re.search('<img src="([^"]+)"',u3.read())
+	if not matches is None:
+		print matches.group(1)
 
-u = urllib.urlopen(url);
-dom = minidom.parseString(u.read());
-
-albumpage = xpath.Evaluate("//Item/DetailPageURL", dom)[0].childNodes[0].nodeValue;
-
-print "Fetching: %s" % albumpage
-y = urllib.urlopen(albumpage);
-
-lines = y.read().split("\n");
-
-ready = 0;
-m = re.compile("^.*img src=\"([^\"]+)\".*$");
-for i in lines:
-	if (ready):
-		m.match(i)
-		print "OK: %s" % m.group(0)
-
-	if (i == "<table border=0 align=left><tr><td valign=top align=center>"):
-		ready = 1
-
-
+if __name__ == "__main__":
+	_lookup({'artist': "sublime", 'album': "sublime"})
 

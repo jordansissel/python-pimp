c6f0c673e28174ca036b8020b146eefb8f0b73f2
Jordan Sissel <jls@semicomplete.com>
2005-12-25 12:29:43 +0000

    - Done.
    git-svn-id: https://semicomplete.googlecode.com/svn/pimp@222 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- test/lameproxy.py
+++ test/lameproxy.py
@@ -4,18 +4,35 @@ from threading import Thread, Event
 import urllib;
 import SocketServer, BaseHTTPServer
 import os
+import select
 
-class LAMEProxy():
-	def __init__(self, port=9000):
+class LAMEProxy:
+	def __init__(self, port=9002):
 		self.port = port
 		self.server = SocketServer.ThreadingTCPServer(('', self.port), self.RequestHandler)
 
 	class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 		def do_GET(self):
-			self.send_header("Content-type", "audio/mpeg");
 			self.send_response(200)
+			self.send_header("Content-type", "audio/mpeg");
 			self.end_headers();
 
-			#(in, out) popen2("lame --mp3input -m m --abr 128 -b 64");
-			#buf = self.rfile.read(4096)
-			print r.path
+			(lamein, lameout) = os.popen2("lame --mp3input -m m --abr 128 -b 64 - -");
+			u = urllib.urlopen("http://kenya.csh.rit.edu:8000%s" % self.path)
+			buf = u.read(4096)
+			while len(buf) > 0:
+				#print "pimp: %d bytes" % len(buf)
+				lamein.write(buf);
+				while 1:
+					data = select.select([lameout.fileno()], [],[],.1);
+					if len(data[0]) == 0:
+						break
+					mp3 = lameout.read(4096)
+					self.wfile.write(mp3)
+					#print "lame: %d bytes" % len(mp3)
+				buf = u.read(4096)
+
+
+if __name__ == "__main__":
+	f = LAMEProxy()
+	f.server.serve_forever()
